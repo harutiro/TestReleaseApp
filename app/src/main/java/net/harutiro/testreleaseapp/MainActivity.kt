ee9ebb2076b173779ceb95f2cc4a70b5cf8b3654
harutiro <harutiro2027@outlook.jp>
2026-05-06 13:36:07 +0900

    version upチェック

--- app/src/main/java/net/harutiro/testreleaseapp/MainActivity.kt
+++ app/src/main/java/net/harutiro/testreleaseapp/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Version App 2 $name!",
         modifier = modifier
     )
 }
